546565e433cb79ad84798a844056760eb24d6cbc
akankshavns <makanksha858@gmail.com>
2025-02-25 20:20:53 +0530

    Initial commit

--- .vscode/ATM.cpp
+++ .vscode/ATM.cpp
@@ -3,34 +3,50 @@ using namespace std;
 
 class ATM{
    int pin, EnteredPin;
-   double TotalBalance = 10000.67;
-   public: void setPin(){
+   double TotalBalance = 500.67;
+
+   public: void createPin(){
       cout<<"Please Enter your Card Pin\t";
       cin>>pin;
-      cout<<"\nYour pin created successfully!";
+      if(pin >= 1000 && pin <= 9999){cout<<"\nYour pin created successfully\n";
+      }else{
+         cout<<"\nPlease enter four digit PIN\n";
+      }
+      
    }
+
    public: void changePin(){
-      int NewPin;
-      cout<<"Enter your pin to reset\t";
-      cin>>NewPin;
-      pin = NewPin;
-      cout<<"Pin changed successfully!";
+      int NewPin,OldPin;
+      cout<<"Enter your PIN";
+      cin>>OldPin;
+      if(pin == OldPin){
+         cout<<"Enter your pin to reset\t";
+         cin>>NewPin;
+         pin = NewPin;
+         cout<<"\nPin changed successfully!n";
+      }else{
+         cout<<"\nIncorrect PIN! Please enter the correct PIN for Reset the PIN\n";
+      }
    }
+
    public: void withdrawMoney(){
       int Amount;
-      cout<<"Enter your 4 digit pin numbr";
+      cout<<"Enter your 4 digit pin number";
       cin>>EnteredPin;
       if(EnteredPin == pin){
          cout << "Enter amount to withdraw: ";
          cin >> Amount;
-         TotalBalance -=Amount;
-         cout << "\nWithdrawal successful! Your new balance: " << TotalBalance << endl;
+         if(TotalBalance > 0 && TotalBalance >= Amount ){
+            TotalBalance -=Amount;
+            cout << "\nWithdrawal successful! Your new balance: " << TotalBalance << endl;
+         }
       }
       else{
-         cout<<"Incorrect PIN! Access denied, Please Enter the correct PIN";
+         cout<<"\nIncorrect PIN! Access denied, Please Enter the correct PIN\n";
       }
    }
-   public: void TransferMoney(double depositAmount){
+
+   public: void depositMoney(double depositAmount){
       cout<<"Enter your 4 digit pin number";
       cin>>EnteredPin;
       if(EnteredPin == pin){
@@ -38,19 +54,20 @@ class ATM{
          cout << "\nBalancr credited successful! Your new balance: " << TotalBalance << endl;
       }
       else{
-         cout<<"Incorrect PIN! Access denied, Please Enter the correct PIN";
+         cout<<"\nIncorrect PIN! Access denied, Please Enter the correct PIN\n";
       }
        
    }
+ 
    public: void BalanceEnquiry(){
       cout<<"Enter your 4 digit pin numbr";
       cin>>EnteredPin;
       if(EnteredPin == pin){
-         cout<<"Your Total Balance is : "<<TotalBalance << endl;
+         cout<<"\nYour Total Balance is : "<<TotalBalance << endl;
       }
       else
       {
-         cout<<"Incorrect PIN! Access denied, Please Enter the correct PIN";
+         cout<<"\nIncorrect PIN! Access denied, Please Enter the correct PIN\n";
       }
       
    }
@@ -62,21 +79,22 @@ class ATM{
 int main(){
    int option;
    ATM obj;
+
    do{
       cout<<"\nChoose Your Option\n";
       cout<<"1. Set the pin.\n";
       cout<<"2. Change the pin.\n";
       cout<<"3. Withdraw money.\n";
-      cout<<"4. Transfer money.\n";
-      cout<<"5. Check Balance.\n";
-      cout<<"6. Exit.\n";
-      cout<<"Enter your option";
+      cout<<"4. Deposit Money.\n";
+      cout<<"6. Check Balance.\n";
+      cout<<"7. Exit.\n";
+      cout<<"Enter your option: ";
       cin>>option;
       
       switch (option)
       {
          case 1:
-            obj.setPin();
+            obj.createPin();
             break;
          case 2:
             obj.changePin();
@@ -88,16 +106,16 @@ int main(){
             double DepositAmount;
             cout<<"Enter the amount for deposit";
             cin>>DepositAmount;
-            obj.TransferMoney(DepositAmount);
+            obj.depositMoney(DepositAmount);
             break;
          case 5:
             obj.BalanceEnquiry();
             break;
          case 6:
-            cout<<"Thank you for using the ATM. Goodbye!\n";
+            cout<<"\nThank you for using the ATM. Goodbye!\n";
             break;
          default:
-            cout<<"Invalid Option! please choose correct option";
+            cout<<"\nInvalid Option! please choose correct option\n";
             break;
       }
    }while (option != 6);
